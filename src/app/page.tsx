91561e91bc0039bb65539ada00e4b16a51bdc9d2
nelson <chinwubanelson@gmail.com>
2025-09-19 07:06:04 +0100

    edit layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <main className="bg-gray-100 w-full min-h-screen pt-[120px] lg:pt-3">
+    <main className="bg-gray-100 w-full min-h-screen pt-[120px] lg:pt-3 overflow-hidden">
       <Hero />
       <WhatWeHaveBuilt />
       <OurFeatures />
